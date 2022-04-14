9c761766c1cfe76a1bba86d224ae1fff367e8a20
Umer Ahmed <84404257+imumer12@users.noreply.github.com>
2022-04-14 19:41:00 +0500

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -1,42 +1,46 @@
 #include<iostream>
 #include<string>
+
 using namespace std;
 
+char table[63] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9' };
+
 void encrypet(char &plain_text) {
-	char table[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 	int temp = 0;
 	int en = 0;
 
-	for (int i = 0; i < 26; i++) {
+	for (int i = 0; i < 63; i++) {
 		if (plain_text == table[i]) {
 			temp = i;
 			break;
 		}
 	}
 
-	en = (temp + 3) % 26;
+	en = (temp + 3) % 63;
 	plain_text = table[en];
-
 }
 
+
 void decrypet(char &plain_text) {
-	char table[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	int temp = 0;
 	int en = 0;
 
-	for (int i = 0; i < 26; i++) {
+	for (int i = 0; i < 63; i++) {
 		if (plain_text == table[i]) {
 			temp = i;
 			break;
 		}
 	}
+
+
 	if (temp - 3 < 0) {
 		en = temp - 3;
-		en = (en + 26) % 26;
+		en = (en + 63) % 63;
 	}
 	else {
-		en = (temp - 3) % 26;
+		en = (temp - 3) % 63;
 	}
+
 	plain_text = table[en];
 }
 
