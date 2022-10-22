b0ea2e946f3fe6751e7a9d74086fad3c58dffecd
Umer Ahmed <84404257+imumer16@users.noreply.github.com>
2022-10-22 18:13:11 +0500

    Add files via upload

---
+++ Bubble sort/Optimised Bubble Sort.cpp
@@ -0,0 +1,20 @@
+void bubbleSort(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        bool flag = 0;
+        for (int j = 0; j < arr.size() - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j + 1], arr[j]);
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+        {
+            cout << " Data Already Sorted " << endl;
+            break;
+        }
+    }
+}
